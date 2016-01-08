6c1b178234532d84ac65d7917303b9b9c93ce763
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-08 08:55:56 +0300

    no message

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,6 +1,6 @@
 <?php
 namespace Dfe\Facebook\Setup;
-use \Magento\Framework\DB\Adapter;
+use Magento\Framework\DB\Adapter;
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
