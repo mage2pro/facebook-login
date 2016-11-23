7cf877a3f2e41bead3bb7665c08ebd8721693a8b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-23 14:15:57 +0300

    no message

--- Button.php
+++ Button.php
@@ -1,38 +1,27 @@
 <?php
 namespace Dfe\FacebookLogin;
 use Dfe\FacebookLogin\Settings as S;
-use Magento\Framework\View\Element\AbstractBlock;
-class Button extends AbstractBlock {
+class Button extends \Df\Sso\Button {
 	/**
-	 * 2016-11-22
+	 * 2016-11-23
 	 * @override
-	 * @see AbstractBlock::_toHtml()
+	 * @see \Df\Sso\Button::loggedOut()
+	 * @used-by \Df\Sso\Button::_toHtml()
 	 * @return string
 	 */
-	protected function _toHtml() {
-		/** @var string $result */
-		/** @var string $result */
-		if (!S::s()->enable()) {
-			$result = '';
-		}
-		else if (df_customer_logged_in()) {
-			$result = '';
-		}
-		else {
-			/** @var string $domId */
-			$domId = df_uid(4, 'dfe-facebook-login-');
-			$result =
-				df_x_magento_init(__CLASS__, 'main', [
-					'domId' => $domId, 'redirect' => $this->getUrl(df_route(__CLASS__))
+	protected function loggedOut() {
+		/** @var string $domId */
+		$domId = df_uid(4, 'dfe-facebook-login-');
+		return
+			df_x_magento_init(__CLASS__, 'main', [
+				'domId' => $domId, 'redirect' => $this->getUrl(df_route(__CLASS__))
+			])
+			.df_tag('li', ['id' => $domId, 'style' => 'display:none'],
+				df_tag('fb:login-button', [
+					'onlogin' => 'dfeFacebookLogin()', 'scope' => 'public_profile,email'
 				])
-				.df_tag('li', ['id' => $domId, 'style' => 'display:none'],
-					df_tag('fb:login-button', [
-						'onlogin' => 'dfeFacebookLogin()', 'scope' => 'public_profile,email'
-					])
-				)
-				.\Df\Facebook\I::init()
-			;
-		}
-		return $result;
+			)
+			.\Df\Facebook\I::init()
+		;
 	}
 }
