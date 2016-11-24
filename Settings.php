caabd5c2073a5c086e353480665c30c13424c484
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-24 03:41:13 +0300

    1.1.13

--- Settings.php
+++ Settings.php
@@ -4,15 +4,15 @@ namespace Dfe\FacebookLogin;
 // Аргумент $s для методов этого класса не нужен,
 // потому что опции этого класса считывается только на витрине для текущего магазина.
 /** @method static Settings s() */
-class Settings extends \Df\Core\Settings {
+class Settings extends \Df\Config\Settings {
 	/**
 	 * 2016-11-23
 	 * @override
-	 * @see \Df\Core\Settings::prefix()
-	 * @used-by \Df\Core\Settings::v()
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
 	 * @return string
 	 */
-	protected function prefix() {return 'dfe_facebook/login/';}
+	protected function prefix() {return 'dfe_facebook/login';}
 }
 
 
--- Settings/Login.php
+++ Settings/Login.php
@@ -1,12 +1,12 @@
 <?php
 namespace Dfe\FacebookLogin\Settings;
 /** @method static Login s() */
-class Login extends \Df\Core\Settings {
+class Login extends \Df\Config\Settings {
 	/**
 	 * @override
-	 * @see \Df\Core\Settings::prefix()
-	 * @used-by \Df\Core\Settings::v()
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
 	 * @return string
 	 */
-	protected function prefix() {return 'dfe_facebook/login/';}
+	protected function prefix() {return 'dfe_facebook/login';}
 }
