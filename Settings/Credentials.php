a26eabbdf8eaac9f079ca3e228483815bf0a0446
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-24 11:06:09 +0300

    no message

--- Settings/Credentials.php
+++ Settings/Credentials.php
@@ -1,7 +1,7 @@
 <?php
 namespace Dfe\FacebookLogin\Settings;
 /** @method static Credentials s() */
-class Credentials extends \Df\Facebook\Settings {
+final class Credentials extends \Df\Facebook\Settings {
 	/** @return string */
 	public function appSecret() {return $this->p('app_secret');}
 }
