198e67e6f21ea48a03fe908bfca2c7f294d4b029
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-25 13:15:28 +0300

    1.1.17

---
+++ Source/Button/Size.php
@@ -0,0 +1,13 @@
+<?php
+// 2016-11-25
+namespace Dfe\FacebookLogin\Source\Button;
+class Size extends \Df\Config\SourceT {
+	/**
+	 * 2016-11-25
+	 * @override
+	 * @see \Df\Config\Source::map()
+	 * @used-by \Df\Config\Source::toOptionArray()
+	 * @return array(string => string)
+	 */
+	protected function map() {return dfa_combine_self(['small', 'medium', 'large', 'xlarge']);}
+}
