59412f6ae3ef89c96309543b9a5e85fd8e4867a9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-02 20:49:03 +0300

    1.2.3

--- Settings.php
+++ Settings.php
@@ -4,7 +4,7 @@ namespace Dfe\FacebookLogin;
 // Аргумент $s для методов этого класса не нужен,
 // потому что опции этого класса считывается только на витрине для текущего магазина.
 /** @method static Settings s() */
-final class Settings extends \Df\Config\Settings {
+final class Settings extends \Df\Sso\Settings {
 	/**
 	 * 2016-11-23
 	 * @override
