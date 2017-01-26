d7c62b28b7287da35b579e238921ad1929ce3857
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-26 17:43:29 +0300

    no message

--- Exception.php
+++ Exception.php
@@ -1,15 +1,15 @@
 <?php
 namespace Dfe\FacebookLogin;
-class Exception extends \Df\Core\Exception {
+final class Exception extends \Df\Core\Exception {
 	/**
 	 * 2015-10-10
 	 * @override
 	 * @see \Df\Core\Exception::message()
 	 * @return string
 	 */
-	public function message() {
-		return "Facebook API error of type {$this->fbType()}: «{$this->fbMessage()}».";
-	}
+	public function message() {return
+		"Facebook API error of type {$this->fbType()}: «{$this->fbMessage()}»."
+	;}
 
 	/** @return string */
 	private function fbMessage() {return $this['message'];}
