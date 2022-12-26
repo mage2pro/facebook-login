0f7444fb7bbaff3f885b2f1bbfdb476e891259fb
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-26 13:50:46 +0000

    refactoring

--- Exception.php
+++ Exception.php
@@ -8,10 +8,5 @@ final class Exception extends \Df\Core\Exception {
 	 * @see \Df\Core\Exception::message()
 	 * @used-by df_xts()
 	 */
-	function message():string {return "Facebook API error of type {$this->fbType()}: «{$this->fbMessage()}».";}
-
-	/** @return string */
-	private function fbMessage() {return $this['message'];}
-	/** @return string */
-	private function fbType() {return $this['type'];}
+	function message():string {return "Facebook API error of type {$this['type']}: «{$this['message']}».";}
 }
