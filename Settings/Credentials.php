437c5c77bd856dc7d50da0257b51f819b1de3ec8
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 00:44:21 +0000

    refactoring

--- Settings/Credentials.php
+++ Settings/Credentials.php
@@ -2,6 +2,9 @@
 namespace Dfe\FacebookLogin\Settings;
 /** @method static Credentials s() */
 final class Credentials extends \Df\Facebook\Settings {
-	/** @used-by \Dfe\FacebookLogin\Customer::req() */
+	/**
+	 * @used-by \Dfe\FacebookLogin\Customer::req()
+	 * @used-by \Dfe\FacebookLogin\Customer::longLivedAccessToken()
+	 */
 	function appSecret():string {return $this->p('app_secret');}
 }
