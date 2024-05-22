be1c1ca55d042c3fd0c3d73601237c6afb3aaf9b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-22 01:00:13 +0100

    https://github.com/mage2pro/core/issues/385

--- ResponseValidator.php
+++ ResponseValidator.php
@@ -6,10 +6,14 @@ final class ResponseValidator extends \Df\API\Response\Validator {
 	/**
 	 * 2015-10-10
 	 * @override
-	 * @see \Df\Core\Exception::message()
-	 * @used-by df_xts()
+	 * @see \Df\API\Response\Validator::long()
+	 * @used-by \Df\API\Client::_p()
+	 * @used-by \Df\API\Exception::message()
 	 */
-	function message():string {return "Facebook API error of type {$this['type']}: «{$this['message']}».";}
+	function long():string {
+		$e = $this->error(); /** @var array(string => string) $e */
+		return "Facebook API error of type {$e['type']}: «{$e['message']}».";
+	}
 
 	/**
 	 * 2024-05-22 "Remove `Df\Core\Exception::$_data`": https://github.com/mage2pro/core/issues/385
@@ -17,5 +21,13 @@ final class ResponseValidator extends \Df\API\Response\Validator {
 	 * @see \Df\API\Response\Validator::valid()
 	 * @used-by \Df\API\Client::_p()
 	 */
-	function valid():bool {return !$this->error() && !$this->errors();}
+	function valid():bool {return !$this->error();}
+
+	/**
+	 * 2024-05-22 "Remove `Df\Core\Exception::$_data`": https://github.com/mage2pro/core/issues/385
+	 * @used-by self::long()
+	 * @used-by self::valid()
+	 * @return array(string => string)|null
+	 */
+	private function error() {return $this->r('error');}
 }
